a77831db7027950c8cfa61fda21a7e5553e9179c
Dmitry <dimka360@mail.ru>
2026-02-23 02:13:22 +0500

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cameraandgeopositiontest.app',
+  appName: 'camera-and-geoposition-react-app',
+  webDir: 'dist'
+};
+
+export default config;
